b63980bb0565a198052fd064ce2cb3a1121337b6
rakoon98 <kun93686@gmail.com>
2025-03-18 22:40:55 +0900

    [learn] $6.12 ApiService 학습습

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter_toonflix_nico_edu/models/webtoon_detail_model.dart';
+import 'package:flutter_toonflix_nico_edu/models/webtoon_episode_model.dart';
 import 'package:flutter_toonflix_nico_edu/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,6 +25,32 @@ class ApiServices {
       }
 
       return webtoonList;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+    if(response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+   static Future<List<WebtoonEpisodeModel>> getLatetstEpisodesById(String id) async {
+    List<WebtoonEpisodeModel> list = [];
+
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+    if(response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for(var episode in episodes) {
+        list.add(WebtoonEpisodeModel.fromJson(episode));  
+      }
+
+      return list;
     }
     throw Error();
   }
